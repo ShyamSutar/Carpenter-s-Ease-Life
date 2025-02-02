fa80808208c5615519070aad7fb9b59e72b5a7da
shyamsutar <shyamsutar629@gmail.com>
2025-02-02 08:25:53 +0530

    flowbite fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { authActions } from "./store/authentication.js";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import "flowbite";
 
 const App = () => {
 
